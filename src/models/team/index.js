da9b7925e3bd99cdb2e6d27a46993355c0b077f8
erick.lima <erick.lima@vileve.com.br>
2023-09-03 00:54:51 -0300

    create model and controller team

---
+++ src/models/team/index.js
@@ -0,0 +1,53 @@
+import DataBase from '../../dataBase/index.js';
+
+export const getList = async payload => {
+  const instance = DataBase.getInstance().data.request();
+
+  let query = 'SELECT * FROM "team"';
+
+  if (payload.status) {
+    query += ' WHERE status = @statusList';
+    instance.input('statusList', payload.status);
+  }
+
+  if (payload.name) {
+    query += payload.status
+      ? ' AND name = @nameList'
+      : ' WHERE name = @nameList';
+    instance.input('nameList', payload.name);
+  }
+  instance.input('status', payload.status);
+  const { recordset, rowsAffected } = await instance.query(query);
+
+  return {
+    status: 'success',
+    message: 'Lista retornada com sucesso.',
+    document: recordset,
+    rowsAffected,
+  };
+};
+
+export const postTeam = async ({ name, status }) => {
+  const instance = DataBase.getInstance().data.request();
+  const query = `insert into "team" ( name, status )
+  values ( @nameTeam, @statusTeam );`;
+
+  instance.input('nameTeam', name);
+  instance.input('statusTeam', status);
+
+  const { rowsAffected } = await instance.query(query);
+
+  if (rowsAffected[0] === 1) {
+    return {
+      status: 'success',
+      message: 'Equipe cadastrada com sucesso.',
+      rowsAffected,
+    };
+  }
+
+  return {
+    status: 'error',
+    message: 'Erro ao cadastrar equipe',
+    rowsAffected,
+  };
+};
