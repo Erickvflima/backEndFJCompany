9aca1e46a286c7258d5ec49ec29a30f9d9326ef6
erick.lima <erick.lima@vileve.com.br>
2023-09-04 14:37:35 -0300

    create get message

---
+++ src/models/message/index.js
@@ -0,0 +1,64 @@
+import DataBase from '../../dataBase/index.js';
+
+export const getList = async payload => {
+  const instance = DataBase.getInstance().data.request();
+
+  let query = 'SELECT * FROM "message"';
+
+  if (payload.status) {
+    query += ' WHERE status = @statusList';
+    instance.input('statusList', payload.status);
+  }
+
+  if (payload.teamId) {
+    query += payload.status
+      ? ' AND teamId = @teamId'
+      : ' WHERE teamId = @teamId';
+    instance.input('teamId', payload.teamId);
+  }
+  instance.input('status', payload.status);
+  const { recordset, rowsAffected } = await instance.query(query);
+
+  return {
+    status: 'success',
+    message: 'Lista retornada com sucesso.',
+    document: recordset,
+    rowsAffected,
+  };
+};
+
+// export const postTeam = async ({ name, status }) => {
+//   const instance = DataBase.getInstance().data.request();
+//   const validNameTeam = await getList({ name: name });
+
+//   if (validNameTeam.rowsAffected[0] === 1) {
+//     return {
+//       status: 'error',
+//       message: 'Nome de equipe já esta cadastrada.',
+//     };
+//   }
+//   const query = `insert into "team" ( name, status )
+//   values ( @nameTeam, @statusTeam );`;
+
+//   instance.input('nameTeam', name);
+//   instance.input('statusTeam', status);
+
+//   const { rowsAffected } = await instance.query(query);
+
+//   const resultTeam = await getList({ name: name });
+
+//   if (rowsAffected[0] === 1) {
+//     return {
+//       status: 'success',
+//       message: 'Equipe cadastrada com sucesso.',
+//       document: resultTeam.document,
+//       rowsAffected,
+//     };
+//   }
+
+//   return {
+//     status: 'error',
+//     message: 'Erro ao cadastrar equipe',
+//     rowsAffected,
+//   };
+// };
